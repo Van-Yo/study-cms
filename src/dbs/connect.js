0b73368b8482be24754bb4a3fb4bf98c4f35c182
wanrong <wanrong@wondersgroup.com>
2019-11-09 13:54:26 +0800

    数据库连接auth修改

--- src/dbs/connect.js
+++ src/dbs/connect.js
@@ -4,7 +4,7 @@
 // 1.导入模块
 const mongoose = require('mongoose');
 // 2.配置连接数据库信息
-mongoose.connect('mongodb://vanlus:82PKIbSxJ0#$rEPO@127.0.0.1:27017/admin',{useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb://root:qwerty787@127.0.0.1:27017/studycms',{useNewUrlParser: true,useUnifiedTopology: true});
 // 3.连接数据库
 let db = mongoose.connection;
 // 4.连接数据库的回调
