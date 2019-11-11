66d4a5fe740ed39a44eedd032bb493e62d055949
wanrong <wanrong@wondersgroup.com>
2019-11-11 14:59:45 +0800

    细节修改和数据库普通用户用户名和密码配置

--- src/dbs/connect.js
+++ src/dbs/connect.js
@@ -4,7 +4,9 @@
 // 1.导入模块
 const mongoose = require('mongoose');
 // 2.配置连接数据库信息
-mongoose.connect('mongodb://jokerwanr:qwerty787@127.0.0.1:27017/studycms',{useNewUrlParser: true,useUnifiedTopology: true});
+//mongoose.connect('mongodb://127.0.0.1:27017/abc',{useNewUrlParser: true,useUnifiedTopology: true});
+// 服务器上的数据库设置了：1.特定的数据库名;2.账号和密码;3.读写的权限
+mongoose.connect('mongodb://vanlus:yinxiaofu799@127.0.0.1:27017/studycms',{useNewUrlParser: true,useUnifiedTopology: true});
 // 3.连接数据库
 let db = mongoose.connection;
 // 4.连接数据库的回调
