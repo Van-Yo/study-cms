7efc1fb355bb2d0c2c3852c9638f06f065c34135
wanrong <wanrong@wondersgroup.com>
2019-11-12 16:31:43 +0800

    用户账号密码的接口

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const userModel = require('../dbs/models/userModel');
+const Mail = require('../utils/mail.js');
+let registerCodeObj = {};
+let resetPsCodeObj = {};
+/**
+ * @api {post} /user/vertiCode 获取注册验证码
+ * @apiName vertiCode
+ * @apiGroup User
+ *
+ * @apiParam {String} us 邮箱
+ *
+ * @apiSuccess {Number} code 0表示成功，-1表示失败
+ * @apiSuccess {String} msg  信息
+ */
+router.post('/vertiCode',(req,res)=>{
+    let {us} = req.body;
+    let code = Math.random().toFixed(6).slice(-6);
+    Mail.mailToAddress(us,code)
+    .then(msg=>{
+        if(msg){
+            registerCodeObj[us] = code;
+            res.send({code:0,msg:'发送成功'});
+        }
+    })
+    .catch(err=>{
+        res.send({code:-1,msg:'发送失败'});
+    })
+})
 /**
  * @api {post} /user/register 用户注册
  * @apiName register
@@ -8,27 +35,110 @@ const userModel = require('../dbs/models/userModel');
  *
  * @apiParam {String} us 用户账号
  * @apiParam {String} ps 用户密码
+ * @apiParam {String} code 验证码
+ *
+ * @apiSuccess {Number} code 0表示成功，-1表示失败
+ * @apiSuccess {String} msg  信息
+ */
+router.post('/register',(req,res)=>{
+    let {us,ps,code} = req.body;
+    if(us && ps && code){
+        if (registerCodeObj[us] == code) {
+            userModel.find({us})
+            .then(msg=>{
+                if(msg.length>0){
+                    res.send({code:-1,msg:'该用户名已被注册'});
+                }else{
+                    return userModel.insertMany({us,ps})
+                }
+            })
+            .then(msg=>{
+                res.send({code:0,msg:'注册成功'});
+            })
+            .catch(err=>{
+                res.send(err);
+            }) 
+        }else{
+            res.send({code:-1,msg:'验证码错误'});
+        }
+    }
+})
+/**
+ * @api {post} /user/login 用户登录
+ * @apiName login
+ * @apiGroup User
+ *
+ * @apiParam {String} us 用户账号
+ * @apiParam {String} ps 用户密码
+ *
+ * @apiSuccess {Number} code 0表示成功，-1表示失败
+ * @apiSuccess {String} msg  信息
+ */
+router.post('/login',(req,res)=>{
+    let {us,ps} = req.body;
+    userModel.find({us,ps}).then(msg=>{
+        if(msg && msg.length>0){
+            res.send({code:0,msg:'ok'});
+        }else{
+            res.send({code:-1,msg:'no'});
+        }
+    })
+})
+/**
+ * @api {post} /user/resetCode 获取重置密码验证码
+ * @apiName resetCode
+ * @apiGroup User
+ *
+ * @apiParam {String} us 邮箱
  *
  * @apiSuccess {Number} code 0表示成功，-1表示失败
  * @apiSuccess {String} msg  
  */
-router.post('/register',(req,res)=>{
-    let {us,ps} = req.body;
-    if(us && ps){
-        userModel.find({us})
-        .then(msg=>{
-            if(msg.length>0){
-                res.send({code:-1,msg:'该用户名已被注册'});
+router.post('/resetCode',(req,res)=>{
+    let {us} = req.body;
+    let code = Math.random().toFixed(6).slice(-6);
+    userModel.find({us})
+    .then(msg=>{
+        if(msg && msg.length>0){
+            return Mail.mailToAddress(us,code);
+        }else{
+            res.send({code:-1,msg:'该用户尚未注册'});
+        }
+    })
+    .then(msg=>{
+        if(msg){
+            resetPsCodeObj[us] = code;
+            res.send({code:0,msg:'发送成功'});
+        }
+    })
+    .catch(err=>{
+        res.send({code:-1,msg:'发送失败'});
+    })
+})
+/**
+ * @api {post} /user/resetPassword 修改密码
+ * @apiName resetPassword
+ * @apiGroup User
+ *
+ * @apiParam {String} us 用户账号
+ * @apiParam {String} ps 用户密码
+ * @apiParam {String} code 验证码
+ *
+ * @apiSuccess {Number} code 0表示成功，-1表示失败
+ * @apiSuccess {String} msg  信息
+ */
+router.post('/resetPassword',(req,res)=>{
+    let {us,ps,code} = req.body;
+    if(us && ps && code==resetPsCodeObj[us]){
+        userModel.update({us},{ps}).then(msg=>{
+            if(msg.nModified==1){
+                res.send({code:0,msg:'修改成功'});
             }else{
-                return userModel.insertMany({us,ps})
+                res.send({code:-1,msg:'修改失败'});
             }
         })
-        .then(msg=>{
-            res.send({code:0,msg:'注册成功'});
-        })
-        .catch(err=>{
-            res.send(err);
-        })
+    }else{
+        res.send({code:-1,msg:'用户名或者密码或者验证码错误'})
     }
 })
 /**
@@ -37,7 +147,7 @@ router.post('/register',(req,res)=>{
  * @apiGroup User
  *
  * @apiSuccess {Number} code 0表示成功，-1表示失败
- * @apiSuccess {Array} msg  
+ * @apiSuccess {Array} msg  数据信息
  */
 router.get('/findAllUsers',(req,res)=>{
     userModel.find().then(msg=>{
